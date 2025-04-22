5c18f82000f44e0895d4caa07a36a3c950e28437
Martin Rodriguez <neomartinr@gmail.com>
2025-04-22 18:53:35 -0300

    banner route changed

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import HomeFeaturedProducts from './HomeFeaturedProducts/HomeFeaturedProducts';
 import HomeFeatures from './HomeFeatures/HomeFeatures';
 import HomeInfo from './HomeInfo/HomeInfo';
-import Banner from '@/layout/Banner/Banner';
+import Banner from '@/Layout/Banner/Banner';
 
 export default function Home() {
   return (
